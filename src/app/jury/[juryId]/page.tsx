f0d33687aa7a1e37cb315021c8f3f5cf1c4d5be1
Kahindo Lwanzo Alfred <alfredlwanzo@gmail.com>
2025-10-21 12:25:15 +0200

    deploy cahnges

--- src/app/jury/[juryId]/page.tsx
+++ src/app/jury/[juryId]/page.tsx
@@ -2,13 +2,11 @@
 
 import { DataFetchErrorResult } from "@/components/errorResult";
 import { useJury } from "@/hooks/useJury";
-import { getJury } from "@/lib/api";
 import { getHSLColor } from "@/lib/utils";
 import { RightOutlined, SafetyCertificateOutlined } from "@ant-design/icons";
-import { useQuery } from "@tanstack/react-query";
 import {
   Avatar,
-  Badge,
+  Button,
   Card,
   Col,
   Divider,
@@ -54,190 +52,217 @@ export default function Page() {
         </Flex>
       </Card>
       <Row gutter={[32, 32]}>
-        <Col xs={24} sm={24} md={8}>
-          <Card loading={isPending}>
-            <Typography.Title level={5}>Membres</Typography.Title>
-            <Typography.Text type="secondary">Président</Typography.Text>
-            {jury && (
-              <List
-                dataSource={[jury.chairperson]}
-                renderItem={(item) => (
-                  <List.Item
-                    extra={
-                      jury?.chairperson?.user.is_active ? (
-                        <Tag
-                          color="green"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Actif
-                        </Tag>
-                      ) : (
-                        <Tag
-                          color="red"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Inactif
-                        </Tag>
-                      )
-                    }
-                  >
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          style={{
-                            backgroundColor: getHSLColor(
-                              `${jury?.chairperson?.user.first_name} ${jury?.chairperson?.user.last_name} ${jury?.chairperson?.user.surname}`
-                            ),
-                          }}
-                        >
-                          {jury?.chairperson?.user.first_name
-                            ?.charAt(0)
-                            .toUpperCase()}
-                          {jury?.chairperson?.user.last_name
-                            ?.charAt(0)
-                            .toUpperCase()}
-                        </Avatar>
-                      }
-                      title={`${jury?.chairperson?.user.first_name} ${jury?.chairperson?.user.last_name} ${jury?.chairperson?.user.surname}`}
-                      description={jury?.chairperson?.academic_title}
-                    />
-                  </List.Item>
-                )}
-              />
-            )}
-            <Divider size="small" />
-            <Typography.Text type="secondary">Sécretaire</Typography.Text>
-            {jury && (
-              <List
-                dataSource={[jury.secretary]}
-                renderItem={(item) => (
-                  <List.Item
-                    extra={
-                      jury?.secretary?.user.is_active ? (
-                        <Tag
-                          color="green"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Actif
-                        </Tag>
-                      ) : (
-                        <Tag
-                          color="red"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Inactif
-                        </Tag>
-                      )
-                    }
-                  >
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          style={{
-                            backgroundColor: getHSLColor(
-                              `${jury?.secretary?.user.first_name} ${jury.secretary?.user.last_name} ${jury.secretary?.user.surname}`
-                            ),
-                          }}
-                        >
-                          {jury?.secretary?.user.first_name
-                            ?.charAt(0)
-                            .toUpperCase()}
-                          {jury?.secretary?.user.last_name
-                            ?.charAt(0)
-                            .toUpperCase()}
-                        </Avatar>
-                      }
-                      title={`${jury.secretary?.user.first_name} ${jury.secretary?.user.last_name} ${jury.secretary?.user.surname}`}
-                      description={jury.secretary?.academic_title}
-                    />
-                  </List.Item>
-                )}
-              />
-            )}
-            <Divider size="small" />
-            <Typography.Text type="secondary">Autres membres</Typography.Text>
-            {jury?.members && jury.members.length > 0 && (
-              <List
-                dataSource={jury?.members}
-                renderItem={(item, index) => (
-                  <List.Item
-                    key={item?.id}
-                    extra={
-                      item.user.is_active ? (
-                        <Tag
-                          color="green"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Actif
-                        </Tag>
-                      ) : (
-                        <Tag
-                          color="red"
-                          bordered={false}
-                          style={{ marginRight: 0 }}
-                        >
-                          Inactif
-                        </Tag>
-                      )
-                    }
-                  >
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          style={{
-                            backgroundColor: getHSLColor(
-                              `${item?.user?.first_name} ${item?.user?.last_name} ${item?.user?.surname}`
-                            ),
-                          }}
-                        >
-                          {item?.user?.first_name?.charAt(0).toUpperCase()}
-                          {item?.user?.last_name?.charAt(0).toUpperCase()}
-                        </Avatar>
-                      }
-                      title={`${item?.user?.first_name} ${item?.user?.last_name} ${item?.user.surname}`}
-                      description={item?.academic_title}
-                    />
-                  </List.Item>
-                )}
-              />
-            )}
-          </Card>
-        </Col>
         <Col xs={24} sm={24} md={16}>
-          <Space style={{ marginBottom: 24 }}>
+          <Space style={{ marginBottom: 16 }}>
             {/* <Badge count={jury?.faculties.length} /> */}
-            <Typography.Title level={3} style={{ marginBottom: 0 }}>
+            <Typography.Title level={5} style={{ marginBottom: 0 }}>
               Filières
             </Typography.Title>
           </Space>
-          <List
-            dataSource={jury?.faculties}
-            renderItem={(item) => (
-              <Link
-                key={item.id}
-                href={`/jury/${juryId}/${item.id}/grade-entry`}
-              >
+          <Card loading={isPending}>
+            <List
+              dataSource={jury?.faculties}
+              renderItem={(item) => (
+                <Link
+                  key={item.id}
+                  href={`/jury/${juryId}/${item.id}/grade-entry`}
+                >
+                  <List.Item
+                    className=" hover:cursor-pointer hover:bg-gray-50"
+                    style={{ paddingLeft: 16, paddingRight: 16 }}
+                    extra={
+                      <Button
+                        color="primary"
+                        variant="dashed"
+                        style={{ boxShadow: "none" }}
+                      >
+                        Gérer
+                      </Button>
+                    }
+                  >
+                    <List.Item.Meta
+                      title={item.acronym}
+                      description={item.name}
+                    />
+                  </List.Item>
+                </Link>
+              )}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} sm={24} md={8}>
+          {/* <Card loading={isPending}> */}
+          <Typography.Title level={5}>Membres</Typography.Title>
+          <Typography.Text type="secondary">Président</Typography.Text>
+          {jury && (
+            <List
+              dataSource={[jury.chairperson]}
+              renderItem={(item) => (
                 <List.Item
-                  className=" hover:cursor-pointer hover:bg-gray-50"
-                  style={{ paddingLeft: 16, paddingRight: 16 }}
-                  // onClick={() => {
-                  //   router.push(`/jury/${juryId}/${item.id}/grade-entry`);
-                  // }}
-                  extra={<RightOutlined />}
+                  extra={
+                    jury?.chairperson?.user.is_active ? (
+                      <Tag
+                        color="green"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Actif
+                      </Tag>
+                    ) : (
+                      <Tag
+                        color="red"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Inactif
+                      </Tag>
+                    )
+                  }
                 >
                   <List.Item.Meta
-                    title={item.acronym}
-                    description={item.name}
+                    avatar={
+                      <Avatar
+                        style={{
+                          backgroundColor: getHSLColor(
+                            `${jury?.chairperson?.user.surname} ${jury?.chairperson?.user.last_name} ${jury?.chairperson?.user.first_name}`
+                          ),
+                        }}
+                      >
+                        {jury?.chairperson?.user.first_name
+                          ?.charAt(0)
+                          .toUpperCase()}
+                      </Avatar>
+                    }
+                    title={`${jury?.chairperson?.user.surname} ${jury?.chairperson?.user.last_name} ${jury?.chairperson?.user.first_name}`}
+                    description={
+                      <Space>
+                        <Typography.Text type="secondary">
+                          Matr.
+                        </Typography.Text>
+                        <Typography.Text>
+                          {jury.chairperson.user.matricule}
+                        </Typography.Text>
+                      </Space>
+                    }
                   />
                 </List.Item>
-              </Link>
-            )}
-          />
+              )}
+            />
+          )}
+          <Divider size="small" />
+          <Typography.Text type="secondary">Sécretaire</Typography.Text>
+          {jury && (
+            <List
+              dataSource={[jury.secretary]}
+              renderItem={(item) => (
+                <List.Item
+                  extra={
+                    jury?.secretary?.user.is_active ? (
+                      <Tag
+                        color="green"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Actif
+                      </Tag>
+                    ) : (
+                      <Tag
+                        color="red"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Inactif
+                      </Tag>
+                    )
+                  }
+                >
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        style={{
+                          backgroundColor: getHSLColor(
+                            `${jury.secretary?.user.surname} ${jury.secretary?.user.last_name} ${jury?.secretary?.user.first_name}`
+                          ),
+                        }}
+                      >
+                        {jury?.secretary?.user.first_name
+                          ?.charAt(0)
+                          .toUpperCase()}
+                      </Avatar>
+                    }
+                    title={`${jury.secretary?.user.surname} ${jury.secretary?.user.last_name} ${jury.secretary?.user.first_name}`}
+                    description={
+                      <Space>
+                        <Typography.Text type="secondary">
+                          Matr.
+                        </Typography.Text>
+                        <Typography.Text>
+                          {jury.secretary.user.matricule}
+                        </Typography.Text>
+                      </Space>
+                    }
+                  />
+                </List.Item>
+              )}
+            />
+          )}
+          <Divider size="small" />
+          <Typography.Text type="secondary">Autres membres</Typography.Text>
+          {jury?.members && jury.members.length > 0 && (
+            <List
+              dataSource={jury?.members}
+              renderItem={(item, index) => (
+                <List.Item
+                  key={item?.id}
+                  extra={
+                    item.user.is_active ? (
+                      <Tag
+                        color="green"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Actif
+                      </Tag>
+                    ) : (
+                      <Tag
+                        color="red"
+                        bordered={false}
+                        style={{ marginRight: 0 }}
+                      >
+                        Inactif
+                      </Tag>
+                    )
+                  }
+                >
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        style={{
+                          backgroundColor: getHSLColor(
+                            `${item?.user?.surname} ${item?.user?.last_name} ${item?.user?.first_name} `
+                          ),
+                        }}
+                      >
+                        {item?.user?.first_name?.charAt(0).toUpperCase()}
+                      </Avatar>
+                    }
+                    title={`${item?.user.surname} ${item?.user?.last_name} ${item?.user?.first_name}`}
+                    description={
+                      <Space>
+                        <Typography.Text type="secondary">
+                          Matr.
+                        </Typography.Text>
+                        <Typography.Text>
+                          {item?.user.matricule}
+                        </Typography.Text>
+                      </Space>
+                    }
+                  />
+                </List.Item>
+              )}
+            />
+          )}
+          {/* </Card> */}
         </Col>
       </Row>
     </div>
